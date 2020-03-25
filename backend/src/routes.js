dc78456421c1009325ed29168835a642d7f42112
Luiz Ribeiro <luiz@tefra.com.br>
2020-03-25 02:49:51 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+
+/**
+ * Métodos HTTP
+ *
+ * GET: Buscar informação no backend
+ * POST: Criar informação no backend
+ * PUT: Atualizar informação no backend
+ * DELETE: Deletar informação no backend
+ * */
+
+/**
+ * Tipos de parâmetros:
+ *
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ * */
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+
+
+module.exports = routes;
